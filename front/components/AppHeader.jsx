d4449fa5862ba853eacf7773d38184a92ac792a8
minhyoungo <59827905+minhyoungo@users.noreply.github.com>
2022-01-14 12:53:28 +0900

    conf

--- front/components/AppHeader.jsx
+++ front/components/AppHeader.jsx
@@ -409,6 +409,241 @@ const AppHeader = ({ children, width }) => {
             </Drawer>
           )}
         </RsWrapper>
+      ) : router.pathname === "/recipeSetting" ? (
+        <RsWrapper bgColor={Theme.basicTheme_C} color={Theme.white_C}>
+          <Wrapper dr={`row`} ju={`space-between`}>
+            <Text fontSize={width < 800 ? `20px` : `26px`} fontWeight={`bold`}>
+              탕전설정
+            </Text>
+            <DotWrapper width={`26px`} onClick={drawarToggle}>
+              <Dot />
+              <Dot margin={`4px 0`} />
+              <Dot />
+            </DotWrapper>
+          </Wrapper>
+          {drawar && (
+            <Drawer
+              placement="right"
+              closable={true}
+              onClose={drawarToggle}
+              visible={drawarToggle}
+              getContainer={false}
+            >
+              <Wrapper>
+                <Wrapper
+                  bgColor={Theme.basicTheme_C}
+                  color={Theme.white_C}
+                  height={`64px`}
+                  borderBottom={`1px solid ${Theme.subTheme_C}`}
+                ></Wrapper>
+                <Wrapper
+                  bgColor={Theme.basicTheme_C}
+                  color={Theme.white_C}
+                  padding={width < 800 ? `25px 20px 40px` : `25px 38px 40px`}
+                >
+                  <Wrapper dr={`row`} ju={`space-between`}>
+                    <Wrapper
+                      width={`auto`}
+                      dr={`row`}
+                      fontSize={width < 800 ? `16px` : `18px`}
+                    >
+                      <Image
+                        alt="profile"
+                        width={`50px`}
+                        height={`50px`}
+                        radius={`100%`}
+                        src={`https://4leaf-s3.s3.ap-northeast-2.amazonaws.com/oneMedic/assets/thumbnail/detail_thumb.png`}
+                      />
+                      <Text
+                        fontSize={`20px`}
+                        fontWeight={`bold`}
+                        margin={`0 2px 0 10px`}
+                      >
+                        홍길동
+                      </Text>
+                      님
+                    </Wrapper>
+                    <Wrapper
+                      width={`auto`}
+                      dr={`row`}
+                      fontSize={`14px`}
+                      color={Theme.grey2_C}
+                    >
+                      <Text margin={`0 10px 0 0`}>계정관리</Text>
+                      <Text>로그아웃</Text>
+                    </Wrapper>
+                  </Wrapper>
+                </Wrapper>
+                <Wrapper padding={width < 800 ? `0 20px` : `0 38px`}>
+                  <Wrapper
+                    radius={`15px`}
+                    bgColor={Theme.white_C}
+                    color={Theme.black_C}
+                    shadow={Theme.shadow_C}
+                    margin={`-20px 0 0`}
+                    padding={width < 800 ? `30px` : `50px`}
+                  >
+                    <Link href={`/promise`}>
+                      <ATag al={`flex-start`} onClick={drawarToggle}>
+                        <Wrapper
+                          dr={`row`}
+                          ju={`flex-start`}
+                          margin={`0 0 20px`}
+                        >
+                          <Image
+                            alt="icon"
+                            width={`26px`}
+                            height={`26px`}
+                            src={`https://4leaf-s3.s3.ap-northeast-2.amazonaws.com/oneMedic/assets/menu_icon/1.jar.png`}
+                          />
+                          <Text
+                            fontSize={width < 800 ? `16px` : `18px`}
+                            margin={`0 0 0 30px`}
+                          >
+                            약속처방
+                          </Text>
+                        </Wrapper>
+                      </ATag>
+                    </Link>
+                    <Wrapper
+                      dr={`row`}
+                      ju={`flex-start`}
+                      padding={`0 0 20px`}
+                      margin={`0 0 20px`}
+                      borderBottom={`1px solid ${Theme.grey2_C}`}
+                    >
+                      <Image
+                        alt="icon"
+                        width={`26px`}
+                        height={`26px`}
+                        src={`https://4leaf-s3.s3.ap-northeast-2.amazonaws.com/oneMedic/assets/menu_icon/2.prescript.png`}
+                      />
+                      <Text
+                        fontSize={width < 800 ? `16px` : `18px`}
+                        margin={`0 0 0 30px`}
+                      >
+                        처방하기
+                      </Text>
+                    </Wrapper>
+                    <Wrapper dr={`row`} ju={`flex-start`} margin={`0 0 20px`}>
+                      <Image
+                        alt="icon"
+                        width={`26px`}
+                        height={`26px`}
+                        src={`https://4leaf-s3.s3.ap-northeast-2.amazonaws.com/oneMedic/assets/menu_icon/3.list.png`}
+                      />
+                      <Text
+                        fontSize={width < 800 ? `16px` : `18px`}
+                        margin={`0 0 0 30px`}
+                      >
+                        주문조회
+                      </Text>
+                    </Wrapper>
+                    <Link href={`/cart`}>
+                      <ATag al={`flex-start`} onClick={drawarToggle}>
+                        <Wrapper
+                          dr={`row`}
+                          ju={`flex-start`}
+                          padding={`0 0 20px`}
+                          margin={`0 0 20px`}
+                          borderBottom={`1px solid ${Theme.grey2_C}`}
+                        >
+                          <Image
+                            alt="icon"
+                            width={`26px`}
+                            height={`26px`}
+                            src={`https://4leaf-s3.s3.ap-northeast-2.amazonaws.com/oneMedic/assets/menu_icon/4.cart.png`}
+                          />
+                          <Text
+                            fontSize={width < 800 ? `16px` : `18px`}
+                            margin={`0 0 0 30px`}
+                          >
+                            장바구니
+                          </Text>
+                        </Wrapper>
+                      </ATag>
+                    </Link>
+                    <Link href={`/notice`}>
+                      <ATag al={`flex-start`} onClick={drawarToggle}>
+                        <Wrapper
+                          dr={`row`}
+                          ju={`flex-start`}
+                          margin={`0 0 20px`}
+                        >
+                          <Image
+                            alt="icon"
+                            width={`26px`}
+                            height={`26px`}
+                            src={`https://4leaf-s3.s3.ap-northeast-2.amazonaws.com/oneMedic/assets/menu_icon/5.megaphone.png`}
+                          />
+                          <Text
+                            fontSize={width < 800 ? `16px` : `18px`}
+                            margin={`0 0 0 30px`}
+                          >
+                            공지사항
+                          </Text>
+                        </Wrapper>
+                      </ATag>
+                    </Link>
+                    <Link href={`/question`}>
+                      <ATag al={`flex-start`} onClick={drawarToggle}>
+                        <Wrapper dr={`row`} ju={`flex-start`}>
+                          <Image
+                            alt="icon"
+                            width={`26px`}
+                            height={`26px`}
+                            src={`https://4leaf-s3.s3.ap-northeast-2.amazonaws.com/oneMedic/assets/menu_icon/6.consult.png`}
+                          />
+                          <Text
+                            fontSize={width < 800 ? `16px` : `18px`}
+                            margin={`0 0 0 30px`}
+                          >
+                            문의하기
+                          </Text>
+                        </Wrapper>
+                      </ATag>
+                    </Link>
+                  </Wrapper>
+                  <Wrapper
+                    radius={`15px`}
+                    bgColor={Theme.white_C}
+                    color={Theme.black_C}
+                    shadow={Theme.shadow_C}
+                    margin={`15px 0 0`}
+                    padding={`30px`}
+                    dr={`row`}
+                  >
+                    <Image
+                      alt="icon"
+                      width={`55px`}
+                      height={`55px`}
+                      src={`https://4leaf-s3.s3.ap-northeast-2.amazonaws.com/oneMedic/assets/menu_icon/public-number.png`}
+                    />
+                    <Wrapper
+                      width={`calc(100% - 55px)`}
+                      al={`flex-start`}
+                      padding={`0 0 0 30px`}
+                    >
+                      <Text
+                        fontSize={width < 800 ? `16px` : `18px`}
+                        fontWeight={`bold`}
+                      >
+                        대표번호
+                      </Text>
+                      <Text
+                        fontSize={width < 800 ? `20px` : `24px`}
+                        fontWeight={`bold`}
+                        color={Theme.basicTheme_C}
+                      >
+                        02-466-1575
+                      </Text>
+                    </Wrapper>
+                  </Wrapper>
+                </Wrapper>
+              </Wrapper>
+            </Drawer>
+          )}
+        </RsWrapper>
       ) : (
         <RsWrapper bgColor={Theme.basicTheme_C} color={Theme.white_C}>
           <Wrapper dr={`row`} ju={`space-between`}>
