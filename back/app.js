b60342fa4cbb904a84f95729d498b2c8e46a6fe5
hmg11123 <=>
2022-05-24 11:44:32 +0900

    modify

--- back/app.js
+++ back/app.js
@@ -61,7 +61,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(morgan(`dev`));
   app.use(
     cors({
-      origin: ["http://localhost:3000", "*"],
+      origin: ["https://modernlab.co.kr", "https://www.modernlab.co.kr", "*"],
       credentials: true,
     })
   );
